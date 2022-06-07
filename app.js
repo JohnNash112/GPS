41bc2f8d6db3e5667a9c1e08ac1a1b46599823f2
Anurag <61429664+JohnNash112@users.noreply.github.com>
2022-06-07 15:52:04 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,99 @@
+// import axios from 'axios';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+const coordlist = [];
+var stop_value = 0;
+
+const send_data = async function() {
+
+  json_type = coordlist.map(points => {
+    return{
+    "timestamp": points[0].toString(),
+    "latitude" : points[1].toString(),
+    "longitude" : points[2].toString(),
+    "speed" : points[3]
+    }
+    
+  });
+  // const myJSON = JSON.stringify(json_type);
+  console.log(json_type)
+  axios.post('http://localhost:8000/analyse', json_type)
+                .then(res => console.log(res))
+  
+  // json_type.forEach(tiny => {
+  //  axios.post('http://localhost:8000/analyse', tiny)
+  //               .then(res => console.log(res))
+  // });
+
+  
+        }
+
+const start = document.querySelector("#start");
+const end = document.querySelector("#end");
+
+
+const getGPS = function () {
+
+      setInterval(() => {
+        if (!stop_value) {
+            navigator.geolocation.watchPosition(
+                data => {
+                    console.log(data);
+                    // var d = new Date(0);
+                    // d.setUTCSeconds(Math.floor(data.timestamp / 1000));
+                    if (data.coords.speed == null) {
+                        coordlist.push([data.timestamp, data.coords.latitude, data.coords.longitude, 0]);
+                    }
+                    else{
+                        coordlist.push([data.timestamp, data.coords.latitude, data.coords.longitude, data.coords.speed]);
+                    }
+                    window.localStorage.setItem("coordinates", JSON.stringify(coordlist.map(String)))
+                },
+                error => console.log(error),
+                {
+                    enableHighAccuracy: true
+                });
+        }
+      }, 1000);  
+        
+};
+const stopGPS = function () {
+    stop_value = 1;
+}
+start.addEventListener("click", getGPS);
+end.addEventListener("click", stopGPS);
+
+const download = function (data) {
+
+    const blob = new Blob([data], { type: 'text/csv' });
+    const url = window.URL.createObjectURL(blob)
+    console.log(url);
+    const a = document.createElement('a')
+    a.setAttribute('href', url)
+
+    a.setAttribute('download', 'download.csv');
+    a.click();
+    a.innerText = 'CSV';
+    // document.body.appendChild(a);
+}
+const get = async function () {
+    var csv_data = coordlist.map(function (d) {
+        return d.join();
+    }).join('\n');
+    download(csv_data);
+}
+
+const score_data = async function (){
+    console.log("Clicked.....")
+    axios.get('http://localhost:8000/score')
+                .then(res => console.log(res))
+
+}
+
+const btn = document.getElementById('action');
+btn.addEventListener('click', get);
+
+const send_btn = document.getElementById('send');
+send_btn.addEventListener('click', send_data);
+
+const score_btn = document.getElementById('score');
+score_btn.addEventListener('click', score_data);
